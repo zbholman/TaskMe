2fcac925e347bddcbb50e1c9ebd7a20a20219c23
zbholman <zbholman@gmail.com>
2016-04-02 11:40:02 -0400

    added removal of tar file to save space

--- install_robo.sh
+++ install_robo.sh
@@ -5,6 +5,8 @@ wget https://download.robomongo.org/0.9.0-rc7/linux/robomongo-0.9.0-rc7-linux-x8
 
 tar -xvf robomongo-0.9.0-rc7-linux-x86_64-2b7a8ca.tar.gz
 
+rm -rf robomongo-0.9.0-rc7-linux-x86_64-2b7a8ca.tar.gz
+
 cd robomongo-0.9.0-rc7-linux-x86_64-2b7a8ca/bin
 
 ./robomongo
